293f973dad4f5ac240c778fd1f17851adb5e4b65
Marina <wernelu1@mail.ru>
2023-12-11 16:38:49 +0300

    Дописан интерфейс матрицы

--- src/lesson13/Main.java
+++ src/lesson13/Main.java
@@ -2,9 +2,9 @@ package lesson13;
 
 public class Main {
     public static void main(String[] args) {
-        double[][] numsForA = {{3, 5, 7, 8},
-                               {1, 3, 6, 8},
-                               {2, 4, 6, 8}};
+        double[][] numsForA = {{3, 6, 7, 8},
+                               {8, 7, 6, 8},
+                               {5, 5, 6, 8}};
         Matrix a = new Matrix(numsForA);
         System.out.println(a.getRows());
         System.out.println(a.getColumns());
@@ -17,13 +17,82 @@ public class Main {
                                {4, 4, 4, 5}};
         Matrix b = new Matrix(numsForB);
 
-        // неявный параметр - это a (внутри метода достапно по this)
-        // явный параметр - это b (внутри метода достапно otherMatrix)
+        // неявный параметр - это a (внутри метода доступно по this)
+        // явный параметр - это b (внутри метода доступно otherMatrix)
         IMatrix resultAdd = a.add(b);
         a.printToConsole();
-        System.out.println("----------------");
-        resultAdd.printToConsole();
-        System.out.println();
+        System.out.println("------------------------");
 
+        resultAdd.printToConsole();
+        System.out.println("------------------------");
+
+        IMatrix resultSub = a.sub(b);
+        resultSub.printToConsole();
+        System.out.println("------------------------");
+
+        double[][] numsForC = {{3, 5, 7},
+                               {1, 3, 6},
+                               {2, 4, 6}};
+        lesson13.Matrix c = new lesson13.Matrix(numsForC);
+
+        double[][] numsForD = {{6, 6, 6},
+                               {5, 5, 5},
+                               {4, 4, 4}};
+        lesson13.Matrix d = new lesson13.Matrix(numsForD);
+
+        System.out.println("Матрица 'C' УМНОЖИТЬ матрица 'D' =");
+        IMatrix resultMull = c.mul(d);
+        resultMull.printToConsole();
+
+        System.out.println("Матрицу 'C' умножить на 3 =");
+        c.mul(3).printToConsole();
+
+        System.out.println("Транспонированная Матрица 'C' =");
+        c.transpose().printToConsole();
+
+        System.out.println("Заполняет Матрицу 'B' числом 1.0 =");
+        b.fillMatrix(1);
+        b.printToConsole();
+
+        System.out.println("Проверяет является ли Матрица 'C' нулевой  =");
+        boolean result = c.isNullMatrix();
+        System.out.println(result);
+
+        System.out.println("Проверяет является ли Матрица 'F' единичной  =");
+        double[][] numsForF = {{1, 0, 0},
+                               {0, 1, 0},
+                               {0, 0, 1}};
+        lesson13.Matrix f = new lesson13.Matrix(numsForF);
+        boolean result1 = f.isIdentityMatrix();
+        System.out.println(result1);
+
+        System.out.println("Проверяет является ли Матрица 'B' квадратной  =");
+        boolean result2 = b.isSquareMatrix();
+        System.out.println(result2);
+
+        System.out.println("Проверяет является ли Матрица 'C' квадратной  =");
+        c.isSquareMatrix();
+
+        System.out.println("Детерминант матрицы 'X' =");
+        double[][] numsForX = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        lesson13.Matrix x = new lesson13.Matrix(numsForX);
+        System.out.println(x.determinant());
+
+        System.out.println("Детерминант матрицы 'Y' =");
+        double[][] numsForY = {{12, 1, 7},
+                {5, 8, 10},
+                {2, 4, 3}};
+        lesson13.Matrix y = new lesson13.Matrix(numsForY);
+        System.out.println(y.determinant());
+
+        System.out.println("Детерминант матрицы 'Z' =");
+        double[][] numsForZ = {{12, 1, 7, 0},
+                {5, 8, 10, 0},
+                {2, 4, 3, 0},
+                {0, 0, 0, 0}};
+        lesson13.Matrix z = new Matrix(numsForZ);
+        System.out.println(z.determinant());
     }
 }
--- src/lesson13/Matrix.java
+++ src/lesson13/Matrix.java
@@ -1,6 +1,6 @@
 package lesson13;
 
-public class Matrix implements IMatrix{
+public class Matrix implements IMatrix {
     private double[][] nums;
 
     public Matrix() {
@@ -11,7 +11,7 @@ public class Matrix implements IMatrix{
         this.nums = nums;
     }
 
-    public Matrix(int row, int col){
+    public Matrix(int row, int col) {
         nums = new double[row][col];
     }
 
@@ -27,11 +27,11 @@ public class Matrix implements IMatrix{
 
     @Override
     public double getValueAt(int rowIndex, int colIndex) throws IndexOutOfBoundsException {
-        if (rowIndex<0 || rowIndex > getRows()){
+        if (rowIndex < 0 || rowIndex > getRows()) {
             System.out.println("rowIndex не подходит!");
             return 0;
         }
-        if (colIndex <0 || colIndex > getColumns()){
+        if (colIndex < 0 || colIndex > getColumns()) {
             System.out.println("rowIndex не подходит!");
             return 0;
         }
@@ -40,12 +40,12 @@ public class Matrix implements IMatrix{
 
     @Override
     public void setValueAt(int rowIndex, int colIndex, double value) throws IndexOutOfBoundsException {
-        if (rowIndex<0 || rowIndex > getRows()){
+        if (rowIndex < 0 || rowIndex > getRows()) {
             System.out.println("rowIndex не подходит!");
             return;
         }
 
-        if (colIndex <0 || colIndex > getColumns()){
+        if (colIndex < 0 || colIndex > getColumns()) {
             System.out.println("rowIndex не подходит!");
             return;
         }
@@ -58,19 +58,19 @@ public class Matrix implements IMatrix{
         // неявный параметр - это a === this (внутри метода доступно по this)
         // явный параметр - это b === otherMatrix (внутри метода доступно otherMatrix)
 
-        if (this.getRows() != otherMatrix.getRows()){
+        if (this.getRows() != otherMatrix.getRows()) {
             System.out.println("Не совпадает количество строк!");
             return null;
         }
-        if (this.getColumns() != otherMatrix.getColumns()){
+        if (this.getColumns() != otherMatrix.getColumns()) {
             System.out.println("Не совпадает количество колонок!");
             return null;
         }
 
         Matrix result = new Matrix(this.getRows(), this.getColumns());
-        for (int i = 0; i < getRows(); i++){
-            for (int j = 0; j < getColumns(); j++){
-                result.setValueAt(i,j,this.getValueAt(i,j) + otherMatrix.getValueAt(i,j));
+        for (int i = 0; i < getRows(); i++) {
+            for (int j = 0; j < getColumns(); j++) {
+                result.setValueAt(i, j, this.getValueAt(i, j) + otherMatrix.getValueAt(i, j));
             }
         }
         return result;
@@ -79,58 +79,166 @@ public class Matrix implements IMatrix{
     @Override
     public IMatrix sub(IMatrix otherMatrix) throws IllegalArgumentException, NullPointerException {
 
-        return null;
+        if (this.getRows() != otherMatrix.getRows()) {
+            System.out.println("Не совпадает количество строк!");
+            return null;
+        }
+        if (this.getColumns() != otherMatrix.getColumns()) {
+            System.out.println("Не совпадает количество колонок!");
+            return null;
+        }
+        Matrix result = new Matrix(this.getRows(), this.getColumns());
+        for (int i = 0; i < getRows(); i++) {
+            for (int j = 0; j < getColumns(); j++) {
+                result.setValueAt(i, j, this.getValueAt(i, j) - otherMatrix.getValueAt(i, j));
+            }
+        }
+        return result;
     }
 
     @Override
     public IMatrix mul(IMatrix otherMatrix) throws IllegalArgumentException, NullPointerException {
-        return null;
+        if (this.getColumns() != otherMatrix.getRows()) {
+            System.out.println("Количество колонок не равно количеству строк!!");
+            return null;
+        }
+        IMatrix resultMatrix = new Matrix(this.getRows(), this.getColumns());
+
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < this.getColumns(); j++) {
+            double sum = 0;
+            for (int k = 0; k < this.getColumns(); k++) {
+                sum += getValueAt(i, k) * otherMatrix.getValueAt(k, j);
+            }
+            resultMatrix.setValueAt(i, j, sum);
+            }
+        }
+        return resultMatrix;
+
     }
+
 
     @Override
     public IMatrix mul(double value) {
-        return null;
+        IMatrix result = new Matrix(this.getRows(), this.getColumns());
+
+        for (int i =0; i< this.getRows(); i++){
+            for (int j = 0; j < this.getColumns(); j++){
+                result.setValueAt(i, j, this.getValueAt(i,j) * value);
+            }
+        }
+        return result;
     }
 
     @Override
-    public IMatrix transpose() {
-        return null;
+    public IMatrix transpose() { // перевернутая матрица
+        Matrix result = new Matrix(this.getColumns(), this.getRows());
+
+        for (int i =0; i < this.getRows(); i++){
+            for (int j = 0; j < this.getColumns(); j++){
+                result.setValueAt(j, i, this.getValueAt(i, j));
+            }
+        }
+        return result;
     }
 
     @Override
     public void fillMatrix(double value) {
-
+        for (int i = 0; i < this.getRows(); i++){
+            for (int j = 0; j < this.getColumns(); j++){
+                this.setValueAt(i, j, value);
+            }
+        }
     }
 
     @Override
     public double determinant() { // найти в инете
-        return 0;
+        if (!isSquareMatrix()) {
+            throw new UnsupportedOperationException("Determinant can only be calculated for square matrices");
+        }
+
+        int n = this.getRows();
+        if (n == 1) {
+            return this.getValueAt(0, 0);
+        } else if (n == 2) {
+            return this.getValueAt(0, 0) * this.getValueAt(1, 1) - this.getValueAt(0, 1) * this.getValueAt(1, 0);
+        } else {
+            double det = 0;
+            for (int i = 0; i < n; i++) {
+                det += this.getValueAt(0, i) * cofactor(0, i).determinant() * ((i % 2 == 0) ? 1 : -1);
+            }
+            return det;
+        }
     }
+    // Helper method to compute the cofactor matrix
+    private IMatrix cofactor(int row, int col) {
+        training.Matrix cofactorMatrix = new training.Matrix(this.getRows() - 1, this.getColumns() - 1);
+        int rowCount = 0;
+        for (int i = 0; i < this.getRows(); i++) {
+            if (i == row) {
+                continue;
+            }
+            int colCount = 0;
+            for (int j = 0; j < this.getColumns(); j++) {
+                if (j == col) {
+                    continue;
+                }
+                cofactorMatrix.setValueAt(rowCount, colCount, this.getValueAt(i, j));
+                colCount++;
+            }
+            rowCount++;
+        }
+        return cofactorMatrix;
+    }
+
 
     @Override
     public boolean isNullMatrix() {
-        return false;
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < this.getColumns(); j++) {
+                if (this.getValueAt(i, j) != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean isIdentityMatrix() {
-        return false;
+        if (this.getRows() != this.getColumns()) {
+            return false; // Not a square matrix, so it cannot be an identity matrix
+        }
+
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < this.getColumns(); j++) {
+                if ((i == j && this.getValueAt(i, j) != 1) || (i != j && this.getValueAt(i, j) != 0)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean isSquareMatrix() {
+        if (this.getRows() == this.getColumns()) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public void printToConsole() {
-       // this - та матрица,которую надо напечатать
+        // this - та матрица,которую надо напечатать
 
-        for (int i = 0; i < this.getRows(); i++){
-            for (int j = 0; j < this.getColumns(); j++){
-                System.out.print(this.getValueAt(i,j) + " ");
+        for (int i = 0; i < this.getRows(); i++) {
+            for (int j = 0; j < this.getColumns(); j++) {
+                System.out.print(this.getValueAt(i, j) + " ");
             }
             System.out.println();
         }
     }
+
+
 }
